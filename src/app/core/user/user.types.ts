9bb74ed46e499b5e53bb5872c28fd9ad9d119d4c
Hezio Cumba <heziocumba@gmail.com>
2026-02-04 18:35:42 +0200

    feat: modify user.types.ts with the api data format

--- src/app/core/user/user.types.ts
+++ src/app/core/user/user.types.ts
@@ -1,7 +1,18 @@
 export interface User {
-    id: string;
-    name: string;
+    id?: string;
+    first_name: string;
+    last_name: string;
     email: string;
+    password: string;
     avatar?: string;
-    status?: string;
+    role: {
+        id?: number;
+        name: "admin" | "user";
+        description?: string;
+        permissions?: {
+            id?: number;
+            name:string;
+            description?: string;
+        }[]
+    }
 }
