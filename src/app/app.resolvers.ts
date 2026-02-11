8feb684247849b0aab7643d6b3b716928e9de1dc
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-02-12 00:36:28 +0200

    refactor: remove unused MessagesService from initialDataResolver

--- src/app/app.resolvers.ts
+++ src/app/app.resolvers.ts
@@ -1,17 +1,14 @@
 import { inject } from '@angular/core';
-import { MessagesService } from 'app/layout/common/messages/messages.service';
 import { NotificationsService } from 'app/layout/common/notifications/notifications.service';
 import { ShortcutsService } from 'app/layout/common/shortcuts/shortcuts.service';
 import { forkJoin } from 'rxjs';
 
 export const initialDataResolver = () => {
-    const messagesService = inject(MessagesService);
     const notificationsService = inject(NotificationsService);
     const shortcutsService = inject(ShortcutsService);
 
     // Fork join multiple API endpoint calls to wait all of them to finish
     return forkJoin([
-        messagesService.getAll(),
         notificationsService.getAll(),
         shortcutsService.getAll(),
     ]);
